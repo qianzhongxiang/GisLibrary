e107d6f64e9347e2f60e196d4b36fedd1e945a76
qianzhongxiang <qianzhongxiang@outlook.com>
2018-11-27 15:20:24 +0800

    add cors request type for historyService

--- projects/cloudy-location/src/lib/history-service/history.service.ts
+++ projects/cloudy-location/src/lib/history-service/history.service.ts
@@ -1,25 +1,25 @@
 import { ConfigurationService } from './../configuration.service';
-import { DeviceService } from './../device-service/device.service';
-import { MapConifg } from './../../utilities/config';
 import { DataItem } from './../../utilities/entities';
 import { Injectable } from '@angular/core';
-import * as jQuery from 'jquery'
+import * as jQuery from 'jquery';
+import { HttpClient } from '@angular/common/http';
 @Injectable()
 export class HistoryService {
-  private callbacks: Array<(data: Array<DataItem>) => void> = []
-  private routeCallbacks: Array<(data: Array<DataItem>) => void> = []
-  private currentUpdates: Array<(item: DataItem, index: number) => void> = []
-  private interval: number = 2000;
-  private intervalFlag: number
-  public Data: Array<DataItem>
-  private currentIndex: number = 0
-  constructor(private ConfigurationService: ConfigurationService) { }
+  private callbacks: Array<(data: Array<DataItem>) => void> = [];
+  private routeCallbacks: Array<(data: Array<DataItem>) => void> = [];
+  private currentUpdates: Array<(item: DataItem, index: number) => void> = [];
+  private interval = 2000;
+  private intervalFlag: number;
+  public Data: Array<DataItem>;
+  private currentIndex = 0;
+  constructor(private configurationService: ConfigurationService,
+    private httpClient: HttpClient) { }
   public Subscribe(callback?: (data: Array<DataItem>) => void
     , currentUpdate?: (item: DataItem, index: number) => void
     , routeCallback?: (data: Array<DataItem>) => void) {
-    if (callback) this.callbacks.push(callback);
-    if (currentUpdate) this.currentUpdates.push(currentUpdate);
-    if (routeCallback) this.routeCallbacks.push(routeCallback);
+    if (callback) { this.callbacks.push(callback); }
+    if (currentUpdate) { this.currentUpdates.push(currentUpdate); }
+    if (routeCallback) { this.routeCallbacks.push(routeCallback); }
   }
   public SetInterval(interval: number) {
     this.interval = interval;
@@ -29,17 +29,17 @@ export class HistoryService {
     this.currentUpdates.forEach(c => c(this.Data[index], index));
   }
   public Launch(index: number = this.currentIndex) {
-    if (!this.Data) return;
+    if (!this.Data) { return; }
     this.Stop();
     let i = index;
     this.intervalFlag = window.setInterval(() => {
-      if (this.Data.length < (i + 1)) return;
+      if (this.Data.length < (i + 1)) { return; }
       this.currentUpdates.forEach(c => c(this.Data[i], i));
       i++;
     }, this.interval);
   }
   public Stop() {
-    if (this.intervalFlag) window.clearInterval(this.intervalFlag);
+    if (this.intervalFlag) { window.clearInterval(this.intervalFlag); }
   }
   public Clean() {
     this.Stop();
@@ -47,44 +47,68 @@ export class HistoryService {
     this.currentIndex = 0;
   }
   /**
-   * 获取历史数据 通过jsonp
+   * 获取历史数据 通过jsonp or access control
    * @param uid 
    * @param type 
    * @param sTime 
    * @param eTime 
    * @param callback 
    */
-  public GetData(datas: Array<{ uid: string, type: string, sTime: string, eTime: string }>, callback?: (data: Array<DataItem>) => void) {
-    this.Clean()
-    let res: Location, url = this.ConfigurationService.MapConfig.webService + `/HistoryGet?callback=?`
-      , dataIndex = 0, index = 1, count = 200;
+  public GetData(datas: Array<{ uid: string, type: string, sTime: string, eTime: string }>,
+    callback?: (data: Array<DataItem>) => void, corsType: 'AccessControl' | 'jsonp' = 'jsonp') {
+    this.Clean();
+    const url = this.configurationService.MapConfig.webService + `/HistoryGet`
+      , index = 1, count = 200;
+    let dataIndex = 0;
 
     // let postdata = { uid: uid, type: type, stime: sTime.toISOString(), etime: eTime.toISOString(), index: 1, count: 200 };
-    let postdata = Object.assign(datas[dataIndex], { index: index, count: count, interval: 15 });// { uid: "352544071943238", type: "SF", stime: sTime.toISOString(), etime: eTime.toISOString(), index: 1, count: 200 };
-    let cb = ((ds: string | Array<DataItem>) => {
-      if (typeof ds === "string") ds = JSON.parse(ds) as Array<DataItem>;
-      let oneMinData = ds.filter(d => d.CustomInterval == 60)
+    let postdata = Object.assign(datas[dataIndex], { index: index, count: count, interval: 15 });
+    // { uid: "352544071943238", type: "SF", stime: sTime.toISOString(), etime: eTime.toISOString(), index: 1, count: 200 };
+    const cb = ((ds: string | Array<DataItem>) => {
+      if (typeof ds === 'string') { ds = JSON.parse(ds) as Array<DataItem>; }
+      const oneMinData = ds.filter(d => d.CustomInterval == 60);
       this.Data = this.Data.concat(oneMinData);
-      if (callback) callback(oneMinData);
+      if (callback) { callback(oneMinData); }
       this.callbacks.forEach(c => c(oneMinData));
       this.routeCallbacks.forEach(c => c(ds as Array<DataItem>));
       if ((ds as Array<any>).length >= postdata.count) {
         postdata.index++;
-        this.Jsonp(url, postdata, cb)
+        this.CorsRequest(corsType, url, postdata, cb, e => {
+          console.log(e);
+        });
       } else if (datas[++dataIndex]) {
         postdata = Object.assign(datas[dataIndex], { index: index, count: count, interval: 15 });
-        this.Jsonp(url, postdata, cb)
+        this.CorsRequest(corsType, url, postdata, cb, e => {
+          console.log(e);
+        });
       }
     }).bind(this);
-    this.Jsonp(url, postdata, cb)
+    this.CorsRequest(corsType, url, postdata, cb, e => {
+      console.log(e);
+    });
     // this.GetRouteData(datas)
     this.Launch();
   }
-  private Jsonp(url: string, data: any, callback: (data: Array<DataItem>) => void) {
+  private CorsRequest(corsType: 'AccessControl' | 'jsonp', url: string, data: any, callback: (data: Array<DataItem>) => void,
+    erroCallback?: (e) => void) {
+    switch (corsType) {
+      case 'AccessControl':
+        this.httpClient.post(url, data).subscribe(callback, erroCallback);
+        break;
+      case 'jsonp':
+        this.Jsonp(`${url}?callback=?`, data, callback, erroCallback);
+        break;
+      default:
+        break;
+    }
+  }
+  private Jsonp(url: string, data: any, callback: (data: Array<DataItem>) => void, erroCallback?: (e) => void) {
     jQuery.ajax(url, {
-      type: "GET", dataType: "jsonp", data: data, success: callback, error: (xhr, s, e) => {
-        console.log("err")
+      type: 'GET', dataType: 'jsonp', data: data, success: callback, error: (xhr, s, e) => {
+        if (erroCallback) {
+          erroCallback(e);
+        }
       }
-    })
+    });
   }
 }
